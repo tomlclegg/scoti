835982da1dee25fff2def28e59dd4ac03eef3b16
Margaret Siple <margaret.siple@noaa.gov>
2023-11-15 13:37:51 +0100

    clean up make_fishing_year_metier_space()

--- make_fishing_year_metier_space.R
+++ make_fishing_year_metier_space.R
@@ -10,7 +10,7 @@
 # time.periods.bycatch<-32:60#if similar as time.periods.fishery
 # hostspot.area<-10 #define area(s) that you would like to be hotspots for bycatch
 # ##
-make_fishing_year_metier <- function(mean.bycatch.event = 1, mean.bycatch.large.event = 20, p.large.event = 0.01,
+make_fishing_year_metier_space <- function(mean.bycatch.event = 1, mean.bycatch.large.event = 20, p.large.event = 0.01,
                                      nboat = 100, mean.fishing.event.boat.day = 2, p.bycatch = c(0.1, .01), p.metier = c(.2, .8),
                                      narea = 10, stochastic = TRUE, spatio.temporal.fishery.trend = TRUE, spatio.temporal.bycatch.trend = TRUE,
                                      spatial.effort.skewness.general = c(1, 1), spatial.effort.skewness.special = c(1.7, 0.3),
@@ -91,84 +91,3 @@ make_fishing_year_metier <- function(mean.bycatch.event = 1, mean.bycatch.large.
   return(fishing)
 }
 
-
-
-#################################################################################################################
-#################################################################################################################
-# look at effort and then introducing high res metier effort reporting bias
-p_reported <- seq(0.1, 1, .1)
-
-effort_bias <- data.frame(year = rep(1:100, each = length(p_reported)), reporting = rep(p_reported, 100), DaS_report = NA, hauls_report = NA, DaS_estimated = NA, hauls_estimated = NA, DaS_real = NA, hauls_real = NA)
-
-m <- 1
-for (j in 1:100) { # replicate 100 years
-  fishing <- make_fishing_year_metier()
-
-  for (i in 1:length(p_reported)) {
-    effort_bias[m, 3:8] <- estimate_fishing_effort(fishing = fishing, p_report = p_reported[i])
-
-    m <- m + 1
-    print(m)
-    flush.console()
-  }
-}
-
-library(ggplot2)
-effort_bias$DaS_report_bias <- (effort_bias$DaS_real - effort_bias$DaS_report) / effort_bias$DaS_real
-effort_bias$DaS_estimated_bias <- (effort_bias$DaS_real - effort_bias$DaS_estimated) / effort_bias$DaS_real
-effort_bias$hauls_report_bias <- (effort_bias$hauls_real - effort_bias$hauls_report) / effort_bias$hauls_real
-effort_bias$hauls_estimated_bias <- (effort_bias$hauls_real - effort_bias$hauls_estimated) / effort_bias$hauls_real
-
-ggplot(effort_bias) +
-  geom_point(aes(x = reporting, y = DaS_report_bias), colour = "blue") +
-  geom_point(aes(x = reporting, y = DaS_estimated_bias), colour = "red") +
-  labs(x = "proportion of the fleet reporting effort", y = "Days at Sea")
-
-
-ggplot(effort_bias, aes(x = reporting, y = DaS_estimated_bias)) +
-  geom_point() +
-  labs(x = "proportion of the fleet reporting effort", y = "Days at Sea")
-
-ggplot(effort_bias, aes(x = reporting, y = DaS_estimated)) +
-  geom_point() +
-  labs(x = "proportion of the fleet reporting effort", y = "Days at Sea")
-
-ggplot(effort_bias, aes(x = reporting, y = hauls_estimated)) +
-  geom_point() +
-  labs(x = "proportion of the fleet reporting effort", y = "number of hauls")
-
-ggplot(effort_bias, aes(x = reporting, y = hauls_estimated_bias)) +
-  geom_point() +
-  labs(x = "proportion of the fleet reporting effort", y = "number of hauls bias")
-
-# note the difference between the variance of DaS and hauls. here we are in a situation where given the haul/day mean, most vessels
-# will go out almost everyday, so the DaS 'extrapolation' is fairly robust, but then the haul number is not because this varies
-# a little between boats
-
-
-
-
-
-# the reporting rate is associated with vessel length
-# what happens if small vessels are not reported, they represent a large proportion of the fleet (say 80%) but also
-# their fishing days differ:
-# option 1: fishing days contain less hauls
-# option 2: they fish in high density areas (closer to shore) so their bycatch rate is higher
-
-#### second premise we deal varying bycatch probability with metier level 6
-
-#### third premise we deteriorate the monitoring programme strata to metier 3
-
-#### fourth premise we deal with bycatch complexity - multiple fisheries and multiple species
-# first PTB and GNS
-
-
-
-
-
-
-####################### 3
-#### 8 Mar 2023
-#### task 1: observer process
-#### task 2: within year trends in variability in fishing effort (mean event per day) and bycatch probability
-#### task 3: introduce "metier" in monitoring and effort function
